f819a6ec9482ff0927055f017ff9b13085e94636
Denilson Baptista <baptistadenilsonbp@gmail.com>
2023-06-19 17:53:54 -0300

    validating data

--- js/script.js
+++ js/script.js
@@ -11,11 +11,22 @@ form.onsubmit = (event) => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if(showAlertError){
+    console.log(showAlertError)
+    return
+  }
+  
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
   Modal.open()
+}
+
+function notANumber(value) {
+  return isNaN(value) || value == ""
 }
 
 function IMC(weight, height) {
